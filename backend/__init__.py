45b116d5cee7141550e3e091f0251d6e97158299
Sanzhar Baratov <SBaratov@one.kz>
2023-02-07 17:37:54 +0600

    added new routes

--- backend/__init__.py
+++ backend/__init__.py
@@ -1,8 +1,23 @@
 from fastapi import FastAPI
+from pydantic import BaseModel
+
+class User(BaseModel):
+    login: str
+    password: str
 
 app = FastAPI()
 
 
 @app.get("/")
 async def root():
-    return {"message": "Hello World"}
+    return {"message": "hello"} 
+
+@app.post("/register")
+async def register(user: User):
+    return user
+
+@app.get("/people")
+async def get_people():
+    return {"message": "people suggestions"}
+
+

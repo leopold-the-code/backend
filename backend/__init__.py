19061bef35eb3ff8273e99ea9735e1d4468efb09
Sanzhar Baratov <SBaratov@one.kz>
2023-03-15 17:02:31 +0600

    Refactored

--- backend/__init__.py
+++ backend/__init__.py
@@ -1,6 +1,8 @@
 from fastapi import FastAPI
+from tortoise import exceptions as db_exceptions
 
 from backend.db import setup_db
+from backend.exception import not_exist_exception_handler
 from backend.routes import router
 from backend.app_events import create_demousers
 from backend.ws import router as ws_router
@@ -11,3 +13,5 @@ setup_db(app)
 app.include_router(router)
 app.include_router(ws_router)
 app.add_event_handler("startup", create_demousers)
+
+app.add_exception_handler(db_exceptions.DoesNotExist, not_exist_exception_handler)
--- backend/exception.py
+++ backend/exception.py
@@ -1,13 +1,7 @@
-from __init__ import app
-from fastapi import Request
-from tortoise import exceptions as db_exceptions
 from fastapi.responses import JSONResponse
 
 
-@app.exception_handler(db_exceptions.DoesNotExist)
-async def not_exist_exception_handler(
-    requset: Request, exc: db_exceptions.DoesNotExist
-):
+async def not_exist_exception_handler(request, exc):
     return JSONResponse(
         status_code=404,
         content={"message": str(exc)},
